d98d67c3e7dd90cb65badd3fd1e27dfc77c66e61
Emily Bowe <ebowe@tcd.ie>
2019-06-02 13:43:41 +0100

    usp Select all students

--- DAL/ShowData.cs
+++ DAL/ShowData.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+using System.Windows.Forms;
 
 namespace DAL
 {
-    class ShowData
+    public class ShowData: DAO
     {
+        DataTable dt = new DataTable();
+        BindingSource bs = new BindingSource();
+        SqlDataAdapter da = new SqlDataAdapter();
+
+        public DataTable Read()
+        {
+            // select all Students with usp
+
+
+            SqlCommand cmd = new SqlCommand("uspAllStudents", OpenCon());
+            cmd.CommandType = CommandType.StoredProcedure;
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            bs.DataSource = dt;
+            return dt;
+
+        }
     }
 }
